089490929ddddd018cdecf835b03525b946dff6e
Fong Ngo <fong.ngo.fn@gmail.com>
2018-02-19 23:43:07 +0100

    make it prettier

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email, Length, DataRequired
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, Required
+
+
+class AddTodoForm(FlaskForm):
+    todo = StringField("Todo", validators=[DataRequired()])
+    addbutton = SubmitField('AddButton')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[Required(), Email(), Length(max=100)])
-    # render <input type="password">
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Send')
-
-class AddTodoForm(FlaskForm):
-    todo = StringField("Todo", validators=[DataRequired()])
-    addbutton = SubmitField('AddButton')
